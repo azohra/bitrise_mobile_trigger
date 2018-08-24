229975263e380bb6e3bf39f471945ff3a0b17e52
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2018-08-24 13:19:25 -0400

    added a second test case to the BitrisePayloadTest file

--- Tests/TriggerTests/BitrisePayloadTest.swift
+++ Tests/TriggerTests/BitrisePayloadTest.swift
@@ -1,16 +1,9 @@
-//
-//  BitrisePayloadTests.swift
-//  MobileTriggerPackageDescription
-//
-//  Created by Azadeh Bagheri (LCL) on 8/20/18.
-//
-
 import XCTest
 @testable import Trigger
 
 class BitrisePayloadTest: XCTestCase {
     
-    func testPayloadObjectToJSON() {
+    func testPayloadObjectConversionToJSON1() {
         let hookInfo = HookInformation(type: "bitrise", apiToken: "AVCTRE$3258gyt65")
         let buildParams = BuildParams(
             branch: "azi_branch",
@@ -48,7 +41,45 @@ class BitrisePayloadTest: XCTestCase {
         }
     }
     
+    func testPayloadObjectConversionToJSON2() {
+        let hookInfo = HookInformation(type: "bitrise", apiToken: "AVCTRE$3258gyt65")
+        let buildParams = BuildParams(
+            branch: "azi_branch",
+            workflowID: "XcodeForever",
+            triggeredBy: "CI",
+            environments: [
+                [
+                    "mapped_to": "API_TEST_ENV",
+                    "value": "This is the test value",
+                    "is_expand": "true"],
+                [
+                    "mapped_to": "HELP_ENV",
+                    "value": "$HOME variable contains user's home directory path",
+                    "is_expand": "false"
+                ]])
+        
+        let hookInfoStr = "\"hook_info\":{\"type\":\"bitrise\",\"api_token\":\"AVCTRE$3258gyt65\"}"
+        let triggeredByStr = "\"triggered_by\":\"CI\""
+        let branchStr = "\"branch\":\"azi_branch\""
+        let workflowIdStr = "\"workflow_id\":\"XcodeForever\""
+        let envVarsStr = "\"environments\":[{\"value\":\"This is the test value\",\"mapped_to\":\"API_TEST_ENV\",\"is_expand\":\"true\"},{\"value\":\"$HOME variable contains user\'s home directory path\",\"mapped_to\":\"HELP_ENV\",\"is_expand\":\"false\"}]"
+        let buildParamsStr = "\"build_params\":{\(workflowIdStr),\(envVarsStr),\(triggeredByStr),\(branchStr)}"
+        let expectedJSON = "{\(hookInfoStr),\(buildParamsStr)}"
+        
+        let payload = BitrisePayload(apiInfo: hookInfo, params: buildParams)
+        let encoder = JSONEncoder()
+        
+        do {
+            let data = try encoder.encode(payload)
+            let result = String(data: data, encoding: .utf8)!
+
+            let errorMsg = "result JSON string doesn't have the expected content.\nresult: \(result)\nexpectedJSON: \(expectedJSON)"
+            XCTAssert(result.contains(hookInfoStr) && result.contains(buildParamsStr), errorMsg)
+        } catch {
+            XCTAssert(false, "\(error)")
+        }
+    }
     static var allTests = [
-        ("testPayloadObjectToJSON", testPayloadObjectToJSON)
+        ("testPayloadObjectConversionToJSON2", testPayloadObjectConversionToJSON2)
     ]
 }
