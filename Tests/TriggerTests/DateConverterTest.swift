c82db3be126d5692a075a3ce05c3376341bd70dd
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2018-12-13 15:14:47 -0500

    modified the error message for the dateConversion test

--- Tests/TriggerTests/DateConverterTest.swift
+++ Tests/TriggerTests/DateConverterTest.swift
@@ -7,8 +7,10 @@ class DateConverterTest: XCTestCase {
         let iso8601Date = "2018-12-12T22:40:38Z"
         let expectedDate = "Dec 12, 2018 at 5:40:38 PM"
         let date = DateConverter.convert(from: iso8601Date)
-        XCTAssert(date == expectedDate, "Date was not coverted properly. Result date is: \(date), while expected date is: \(expectedDate)")
-    }
+        XCTAssert(date == expectedDate, """
+            Date was not coverted properly. Result date is: \(date), while expected date is: \(expectedDate)
+            """
+        )}
     
     static var allTests = [
         ("testDateConversion", testDateConversion)
