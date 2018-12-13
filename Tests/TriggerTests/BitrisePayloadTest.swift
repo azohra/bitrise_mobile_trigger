0edcffbef39d5dd4eee968d1e08654c3db8b8f59
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2018-12-13 14:23:26 -0500

    added the BitrisePayloadTest to the list of linux tests as well

--- Tests/TriggerTests/BitrisePayloadTest.swift
+++ Tests/TriggerTests/BitrisePayloadTest.swift
@@ -27,6 +27,7 @@ class BitrisePayloadTest: XCTestCase {
         let triggeredByStr = "\"triggered_by\":\"CI\""
         let branchStr = "\"branch\":\"azi_branch\""
         let workflowIdStr = "\"workflow_id\":\"XcodeForever\""
+        //order of the fileds in the Json string produced using the JSONEncoder() changes. Below are the two possible outputs:
         let envVarsStr1 = "\"environments\":[{\"mapped_to\":\"API_TEST_ENV\",\"value\":\"This is the test value\",\"is_expand\":\"true\"},{\"mapped_to\":\"HELP_ENV\",\"value\":\"$HOME variable contains user\'s home directory path\",\"is_expand\":\"false\"}]"
         let envVarsStr2 = "\"environments\":[{\"value\":\"This is the test value\",\"mapped_to\":\"API_TEST_ENV\",\"is_expand\":\"true\"},{\"value\":\"$HOME variable contains user\'s home directory path\",\"mapped_to\":\"HELP_ENV\",\"is_expand\":\"false\"}]"
         let buildParamsStr1 = "\"build_params\":{\(workflowIdStr),\(envVarsStr1),\(triggeredByStr),\(branchStr)}"
@@ -46,8 +47,7 @@ class BitrisePayloadTest: XCTestCase {
         }
     }
     
-    func assertResult(resultString: String) {
-        
-        
-    }
+    static var allTests = [
+        ("testPayloadObjectConversionToJSON", testPayloadObjectConversionToJSON)
+    ]
 }
--- Tests/TriggerTests/XCTestManifests.swift
+++ Tests/TriggerTests/XCTestManifests.swift
@@ -3,6 +3,7 @@ import XCTest
 #if os(Linux)
 public func allTests() -> [XCTestCaseEntry] {
     return [
+        testCase(BitrisePayloadTest.allTests),
         testCase(BuildStatusMessageTest.allTests),
         testCase(ConfigurationTest.allTests),
         testCase(HTTPRequestTest.allTests),
