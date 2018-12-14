7452ee619878421e3b83651a716220a600fb03fb
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2018-12-14 11:37:59 -0500

    changed the DateConverter and its unit test to work properly on both platforms

--- Tests/TriggerTests/DateConverterTest.swift
+++ Tests/TriggerTests/DateConverterTest.swift
@@ -5,7 +5,7 @@ class DateConverterTest: XCTestCase {
 
     func testDateConversion() {
         let iso8601Date = "2018-12-12T22:40:38Z"
-        let expectedDate = "Dec 12, 2018 at 5:40:38 PM"
+        let expectedDate = "December 12, 2018 at 5:40:38 PM EST"
         let date = DateConverter.convert(from: iso8601Date)
         XCTAssert(date == expectedDate, """
             Date was not coverted properly. Result date is: \(date), while expected date is: \(expectedDate)
