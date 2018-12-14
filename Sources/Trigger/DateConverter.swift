8b06ac4a56aafc56ae3a7448dd61add9fc122950
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2018-12-14 11:37:59 -0500

    changed the DateConverter and its unit test to work properly on both platforms

--- Sources/Trigger/DateConverter.swift
+++ Sources/Trigger/DateConverter.swift
@@ -4,13 +4,17 @@ public struct DateConverter {
     
     public static func convert(from iso8601Date: String) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
-        guard let dateRepresentation = dateFormatter.date(from: iso8601Date) else {
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        guard let dateObject = dateFormatter.date(from: iso8601Date) else {
             print ("Date object creation was not successful")
             exit(1)
         }
-        let currentDate = DateFormatter.localizedString(
-            from: dateRepresentation, dateStyle: .medium, timeStyle: .medium)
+        let dateFormatter2 = DateFormatter()
+        dateFormatter2.timeZone = TimeZone(abbreviation: "EST")
+        dateFormatter2.timeStyle = .long
+        dateFormatter2.dateStyle = .long
+        let currentDate = dateFormatter2.string(from: dateObject)
         return currentDate
     }
 }
