24a271e40c06a4daa385d1694a34d99fd27eba1f
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2019-03-21 16:48:33 -0400

    ME-7 Added a private function called handleRequest() to the BitriseClient.swift file to handle the http requests Added a timeout mechanism to the API calls to retry to reach the Bitrise API if the response status code is 503 Modified indentations in some of the files

--- Sources/MobileTrigger/main.swift
+++ Sources/MobileTrigger/main.swift
@@ -31,8 +31,8 @@ parser.flag("-V", "--version", "Version of cli running.", standAlone: true)
 
 let cliMap = parser.parse(&CommandLine.arguments)
 if cliMap["-V"] != nil {
-  print("Trigger version: ", App.version.rawValue)
-  exit(0)
+    print("Trigger version: ", App.version.rawValue)
+    exit(0)
 }
 
 guard let configPath = cliMap["-c"] as? String else {
@@ -43,18 +43,18 @@ guard let configPath = cliMap["-c"] as? String else {
 // Read the config file and create config object
 let json: Data
 do {
-  json = try Config.readConfigFile(path: configPath)
+    json = try Config.readConfigFile(path: configPath)
 } catch {
-  print(error)
-  exit(1)
+    print(error)
+    exit(1)
 }
 
 let projectConfig: Config
 do {
-  projectConfig = try Config(with: json)
+    projectConfig = try Config(with: json)
 } catch {
-  print(error)
-  exit(1)
+    print(error)
+    exit(1)
 }
 
 // create bitrise client object
@@ -68,99 +68,99 @@ var bitriseClient = BitriseClient(projectConfig: projectConfig)
 // --------------------------------------------------------------------------------------------
 // hit bitrise api to trigger the build and parse response
 if let branch = cliMap["-b"] as? String, let workflowId = cliMap["-w"] as? String {
-  let (response, responseError) = bitriseClient.triggerWorkflow(
+    let (response, responseError) = bitriseClient.triggerWorkflow(
     branch: branch,
     workflowId: workflowId,
     envs: cliMap["-e"] as? String
-  )
+    )
     
-  guard let triggerResponse = response else {
-    print("!ERROR:", responseError ?? "Both response and responseError are nil. This should not occur.")
-    exit(1)
-  }
-    
-  if triggerResponse.status != "ok" {
-    print(":> API call to trigger Bitrise did NOT return with `ok` status. Trigger failed.")
-
-    if let slackURL = projectConfig.slackURL {
-      switch SlackClient(slackURL: slackURL)
-        .sendTriggerFailedMessageToSlack(planName: "Bitrise Plan Name goes here", workflowId: "UnitTest") {
-      case .success:
-        print("error message sent to slack")
-      case .failure:
-        print("Could not reach slack to give error message that trigger failed")
-      }
-    }
-
-     exit(1)
-  }
-
-  print(":> Waiting on build...")
-
-  let buildURL = triggerResponse.buildURL
-  let buildSlug = triggerResponse.buildSlug
-  var buildStatus: Int = 0
-  var previousBuildStatusText: String?
-  var wasStartTimePrinted = false
-    
-  print("Build URL: ", buildURL)
-   // TODO: Implement a timeout capability
-  
-  while buildStatus == 0 {
-    sleep(5)
-    guard let res = bitriseClient.checkBuildStatus(slug: buildSlug) else {
-        print(":!ERROR - checkBuildStatus returned nil")
+    guard let triggerResponse = response else {
+        print("!ERROR:", responseError ?? "Both response and responseError are nil. This should not occur.")
         exit(1)
     }
-    let currentBuildStatusText = res.data.statusText
-    if previousBuildStatusText != currentBuildStatusText {
-        print("Build", res.data.statusText)
-        previousBuildStatusText = currentBuildStatusText
-    }
-    buildStatus = res.data.status
-    if let buildStartTime = res.data.startedOnWorkerAt, !wasStartTimePrinted {
-       let date = DateConverter.convert(from: buildStartTime)
-       print("Build start time: ", date)
-       wasStartTimePrinted = true
-    }
-  }
     
-  // Fetch the build logs
-  // Polling is done before the end point is called to give bitrise time to make the build logs available
-  // Max. wait time is the pollingInterval times the number of retries, e.g. 5s x 4 retries gives a 20s max. waiting time
-  var logIsArchived = false
-  var responseFromGetLogInfo: BitriseLogInfoResponse?
-  var counter = 0
-  let retry = 4
-  let pollingInterval: UInt32 = 5
-    
-  while !logIsArchived && counter < retry {
-    sleep(pollingInterval)
-    responseFromGetLogInfo = bitriseClient.getLogInfo(slug: buildSlug)
-    counter += 1
-    guard let response = responseFromGetLogInfo else {
-      print(":!ERROR - getLogInfo returned nil")
-      exit(1)
+    if triggerResponse.status != "ok" {
+        print(":> API call to trigger Bitrise did NOT return with `ok` status. Trigger failed.")
+        
+        if let slackURL = projectConfig.slackURL {
+            switch SlackClient(slackURL: slackURL)
+                .sendTriggerFailedMessageToSlack(planName: "Bitrise Plan Name goes here", workflowId: "UnitTest") {
+            case .success:
+                print("error message sent to slack")
+            case .failure:
+                print("Could not reach slack to give error message that trigger failed")
+            }
+        }
+        
+        exit(1)
     }
-    logIsArchived = response.isArchived
-  }
 
-  let logs = bitriseClient.getLogs(from: responseFromGetLogInfo!)
-  if let logs = logs {
-    // Append the build logs to stdout
-    print("================================================================================")
-    print("============================== Bitrise Logs Start ==============================")
-    print(logs)
-    print("================================================================================")
-    print("==============================  Bitrise Logs End  ==============================")
-  } else {
-    print("LOGS WERE NOT AVAILABLE - go to \(buildURL) to see log.")
-  }
+    print(":> Waiting on build...")
 
-  let msg = bitriseClient.buildStatusMessage(buildStatus)
-  print(msg)
+    let buildURL = triggerResponse.buildURL
+    let buildSlug = triggerResponse.buildSlug
+    var buildStatus: Int = 0
+    var previousBuildStatusText: String?
+    var wasStartTimePrinted = false
+    
+    print("Build URL: ", buildURL)
+    // TODO: Implement a timeout capability
+  
+    while buildStatus == 0 {
+        sleep(5)
+        guard let res = bitriseClient.checkBuildStatus(slug: buildSlug) else {
+            print(":!ERROR - checkBuildStatus returned nil")
+            exit(1)
+        }
+        let currentBuildStatusText = res.data.statusText
+        if previousBuildStatusText != currentBuildStatusText {
+            print("Build", res.data.statusText)
+            previousBuildStatusText = currentBuildStatusText
+        }
+        buildStatus = res.data.status
+        if let buildStartTime = res.data.startedOnWorkerAt, !wasStartTimePrinted {
+            let date = DateConverter.convert(from: buildStartTime)
+            print("Build start time: ", date)
+            wasStartTimePrinted = true
+        }
+    }
+    
+    // Fetch the build logs
+    // Polling is done before the end point is called to give bitrise time to make the build logs available
+    // Max. wait time is the pollingInterval times the number of retries, e.g. 5s x 4 retries gives a 20s max. waiting time
+    var logIsArchived = false
+    var responseFromGetLogInfo: BitriseLogInfoResponse?
+    var counter = 0
+    let retry = 4
+    let pollingInterval: UInt32 = 5
+    
+    while !logIsArchived && counter < retry {
+        sleep(pollingInterval)
+        responseFromGetLogInfo = bitriseClient.getLogInfo(slug: buildSlug)
+        counter += 1
+        guard let response = responseFromGetLogInfo else {
+            print(":!ERROR - getLogInfo returned nil")
+            exit(1)
+        }
+        logIsArchived = response.isArchived
+    }
 
-  let success = buildStatus == 1
-  let exitCode: Int32 = success ? 0 : 1
-  exit(exitCode)
+    let logs = bitriseClient.getLogs(from: responseFromGetLogInfo!)
+    if let logs = logs {
+        // Append the build logs to stdout
+        print("================================================================================")
+        print("============================== Bitrise Logs Start ==============================")
+        print(logs)
+        print("================================================================================")
+        print("==============================  Bitrise Logs End  ==============================")
+    } else {
+        print("LOGS WERE NOT AVAILABLE - go to \(buildURL) to see log.")
+    }
+
+    let msg = bitriseClient.buildStatusMessage(buildStatus)
+    print(msg)
+
+    let success = buildStatus == 1
+    let exitCode: Int32 = success ? 0 : 1
+    exit(exitCode)
 }
--- Sources/Trigger/BitriseClient.swift
+++ Sources/Trigger/BitriseClient.swift
@@ -1,226 +1,219 @@
 import Foundation
 
 enum HttpMethod: String {
-  case get = "GET"
-  case post = "POST"
+    case get = "GET"
+    case post = "POST"
 }
 public enum TriggerError: Error {
     case badKeyValueFormat(String)
     case emptyResponse(String)
 }
 public struct BitriseClient {
-  let delegate: HTTPRequestEngine
-  let theAccessToken: String
-  let token: String
-  let slug: String
-  
-  public init(projectConfig: Config) {
-    self.theAccessToken = projectConfig.theAccessToken
-    self.token = projectConfig.token
-    self.slug = projectConfig.slug
-    self.delegate = HTTPRequest()
-  }
+    let delegate: HTTPRequestEngine
+    let theAccessToken: String
+    let token: String
+    let slug: String
+    
+    public init(projectConfig: Config) {
+        self.theAccessToken = projectConfig.theAccessToken
+        self.token = projectConfig.token
+        self.slug = projectConfig.slug
+        self.delegate = HTTPRequest()
+    }
 }
 
 extension BitriseClient {
-  private func bitrisePayload(branch: String, workflowId: String, envs: String?) throws -> Data {
-    let hookInfo = HookInformation(type: "bitrise", apiToken: token)
-    let buildParams = BuildParams(
-        branch: branch,
-        workflowID: workflowId,
-        triggeredBy: "CI",
-        environments: try convertToEnvArray(from: envs)
+    private func bitrisePayload(branch: String, workflowId: String, envs: String?) throws -> Data {
+        let hookInfo = HookInformation(type: "bitrise", apiToken: token)
+        let buildParams = BuildParams(
+            branch: branch,
+            workflowID: workflowId,
+            triggeredBy: "CI",
+            environments: try convertToEnvArray(from: envs)
         )
-    let payload = BitrisePayload(apiInfo: hookInfo, params: buildParams)
-    let encoder = JSONEncoder()
-    let data = try encoder.encode(payload)
-//    For logging
-//    let _ = String(data: data, encoding: .utf8)!
-    return data
-  }
-  
-  private func convertToEnvArray(from envStr: String?) throws -> [[String: String]] {
-    guard let envStr = envStr else { return [] }
-    let envArray: [[String: String]] = try envStr.components(separatedBy: ",").map {
-        let separatedKeyValueArray = $0.components(separatedBy: "=")
-        if separatedKeyValueArray.count < 2 {
-            throw TriggerError.badKeyValueFormat("key-value pairs should be passed in the form of key=value")
-        }
-      return ["mapped_to": "\(separatedKeyValueArray[0])", "value": "\(separatedKeyValueArray[1])", "is_expand": "true"]
+        let payload = BitrisePayload(apiInfo: hookInfo, params: buildParams)
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(payload)
+        //    For logging
+        //    let _ = String(data: data, encoding: .utf8)!
+        return data
     }
-    return envArray
-  }
+  
+    private func convertToEnvArray(from envStr: String?) throws -> [[String: String]] {
+        guard let envStr = envStr else { return [] }
+        let envArray: [[String: String]] = try envStr.components(separatedBy: ",").map {
+            let separatedKeyValueArray = $0.components(separatedBy: "=")
+            if separatedKeyValueArray.count < 2 {
+                throw TriggerError.badKeyValueFormat("key-value pairs should be passed in the form of key=value")
+            }
+            return ["mapped_to": "\(separatedKeyValueArray[0])", "value": "\(separatedKeyValueArray[1])", "is_expand": "true"]
+        }
+        return envArray
+    }
 }
 
 extension BitriseClient {
-  public func triggerWorkflow(branch: String, workflowId: String, envs: String?) -> (BitriseTriggerResponse?, Error?) {
-    let triggerEndpoint = "https://app.bitrise.io/app/\(slug)/build/start.json"
+    public func triggerWorkflow(branch: String, workflowId: String, envs: String?) -> (BitriseTriggerResponse?, Error?) {
+        let triggerEndpoint = "https://app.bitrise.io/app/\(slug)/build/start.json"
+        
+        // create the payload for the http call
+        var payload: Data
+        do {
+            payload = try bitrisePayload(branch: branch, workflowId: workflowId, envs: envs)
+        } catch {
+            return (nil, error)
+        }
+        
+        // build the request
+        let request = delegate.request(
+            url: triggerEndpoint,
+            method: .post,
+            headers: ["Content-Type": "application/json"],
+            body: payload)
+
+        // send the request
+        let (responseData, _, responseError) = delegate.sendRequest(request: request)
+        
+        if let err = responseError {
+            return (nil, err)
+        } else {
+            
+            // TODO: log response
+            
+            // APIs usually respond with the data you just sent in your POST request
+            guard let data = responseData, String(data: data, encoding: .utf8) != nil else {
+                return (nil, TriggerError.emptyResponse("no readable data received in response"))
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let res = try decoder.decode(BitriseTriggerResponse.self, from: data)
+                return (res, nil)
+            } catch {
+                return (nil, error)
+            }
+        }
+    }
+  
+    // Documented at: https://devcenter.bitrise.io/api/v0.1/#get-appsapp-slugbuildsbuild-slug
+    public func checkBuildStatus(slug buildSlug: String) -> BitriseBuildResponse? {
+        let endpoint = "https://api.bitrise.io/v0.1/apps/\(slug)/builds/\(buildSlug)"
+        let headers = ["Content-Type": "application/json", "Authorization": theAccessToken]
+        let request = delegate.request(url: endpoint, method: .get, headers: headers, body: nil)
+        let responseData = handleRequest(request: request)
+        
+        // APIs usually respond with the data you just sent in your GET request
+        guard let data = responseData, String(data: data, encoding: .utf8) != nil else {
+            print("no readable data received in response")
+            return nil
+        }
+        // For logging
+        // print("response: ", utf8Representation)
+        
+        do {
+            let decoder = JSONDecoder()
+            let res = try decoder.decode(BitriseBuildResponse.self, from: data)
+            return res
+        } catch {
+            print(error)
+            return nil
+        }
+    }
     
-    // create the payload for the http call
-    
-    var payload: Data
-    do {
-      payload = try bitrisePayload(branch: branch, workflowId: workflowId, envs: envs)
-    } catch {
-        return (nil, error)
+    // Documented at http://devcenter.bitrise.io/api/v0.1/#get-appsapp-slugbuildsbuild-sluglog
+    public func getLogInfo(slug buildSlug: String) -> BitriseLogInfoResponse? {
+        let endpoint = "https://api.bitrise.io/v0.1/apps/\(slug)/builds/\(buildSlug)/log"
+        let headers = ["Content-Type": "application/json", "Authorization": theAccessToken]
+        
+        let request = delegate.request(url: endpoint, method: .get, headers: headers, body: nil)
+        let responseData = handleRequest(request: request)
+        
+        // APIs usually respond with the data you just sent in your GET request
+        guard let data = responseData, String(data: data, encoding: .utf8) != nil else {
+            print("no readable data received in response")
+            return nil
+        }
+        // For logging
+        // print("response: ", utf8Representation)
+        
+        do {
+            let decoder = JSONDecoder()
+            let res = try decoder.decode(BitriseLogInfoResponse.self, from: data)
+            return res
+        } catch {
+            print(error)
+            return nil
+        }
     }
 
-    // build the request
-    let request = delegate.request(
-        url: triggerEndpoint,
-        method: .post,
-        headers: ["Content-Type": "application/json"],
-        body: payload)
-    
-    // send the request
-    let (responseData, _, responseError) = delegate.sendRequest(request: request)
-    
-    if let err = responseError {
-        return (nil, err)
-    } else {
-    
-        // TODO: log response
-    
-        // APIs usually respond with the data you just sent in your POST request
-        guard let data = responseData, String(data: data, encoding: .utf8) != nil else {
-          return (nil, TriggerError.emptyResponse("no readable data received in response"))
+    // No special headers should be added for the request to the expiring_raw_log_url.
+    // See http://devcenter.bitrise.io/api/v0.1/#get-appsapp-slugbuildsbuild-sluglog
+    public func getLogs(from logInfo: BitriseLogInfoResponse) -> String? {
+        guard let url = logInfo.expiringRawLogURL else {
+            print(":!ERROR - Could not get the expiring raw log url!")
+            exit(1)
         }
-    
-        do {
-          let decoder = JSONDecoder()
-          let res = try decoder.decode(BitriseTriggerResponse.self, from: data)
-          return (res, nil)
-        } catch {
-          return (nil, error)
+        let request = delegate.request(url: url, method: .get, headers: nil, body: nil)
+        let responseData = handleRequest(request: request)
+        // APIs usually respond with the data you just sent in your GET request
+        if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
+            return utf8Representation
+        } else {
+            print("no readable data received in response")
+            return nil
         }
     }
-  }
-  
-  // Documented at: https://devcenter.bitrise.io/api/v0.1/#get-appsapp-slugbuildsbuild-slug
-  public func checkBuildStatus(slug buildSlug: String) -> BitriseBuildResponse? {
-    let endpoint = "https://api.bitrise.io/v0.1/apps/\(slug)/builds/\(buildSlug)"
-    let headers = ["Content-Type": "application/json", "Authorization": theAccessToken]
     
-    let request = delegate.request(url: endpoint, method: .get, headers: headers, body: nil)
-    let (responseData, response, responseError) = delegate.sendRequest(request: request)
-    
-    guard responseError == nil else {
-        print(responseError!)
-        exit(1)
+    // # Status codes (status) and related status texts (status_text):
+    // # status=0: Not finished yet.
+    // # If is_on_hold = true: the build did not start yet (status_text=on-hold)
+    // # If is_on_hold = false: the build is running (status_text=in-progress).
+    // # status=1: Build finished, with success (status_text=success).
+    // # status=2: Build finished, with error (status_text=error).
+    // # status=3: Build was aborted (status_text=aborted).
+    public  func buildStatusMessage(_ status: Int) -> String {
+        switch status {
+        case 0:  return "Build TIMED OUT base on mobile trigger internal setting"
+        case 1:  return "Build finished, with success"
+        case 2:  return "Build finished, with error"
+        case 3:  return "Build was aborted"
+        default: return "Build status did not match any of the values 0, 1, 2 or 3.\n This is very strange!"
+        }
     }
     
-    if let res = response as? HTTPURLResponse {
-      let returnCode = res.statusCode
-      if returnCode != 200 { print("Response code for checkBuildStatus was ", returnCode) }
-    } else {
-        print("response returned nil")
-    }
-      
-      // APIs usually respond with the data you just sent in your GET request
-    guard let data = responseData, String(data: data, encoding: .utf8) != nil else {
-      print("no readable data received in response")
-      return nil
-    }
-        // For logging
-        // print("response: ", utf8Representation)
+    private func handleRequest(request: URLRequest) -> Data? {
+        var responseData: Data?
+        var counter = 0
+        let retry = 4
+        let pollingInterval: UInt32 = 15
         
-    do {
-      let decoder = JSONDecoder()
-      let res = try decoder.decode(BitriseBuildResponse.self, from: data)
-      return res
-    } catch {
-      print(error)
-      return nil
+        while counter < retry {
+            let (responseBody, response, responseError) = delegate.sendRequest(request: request)
+            counter += 1
+            guard responseError == nil else {
+                print(responseError!)
+                exit(1)
+            }
+            guard let res = response as? HTTPURLResponse else {
+                print("HTTPURLResponse returned nil")
+                exit(1)
+            }
+            let returnCode = res.statusCode
+            
+            if returnCode == 503 {
+                print("Attempted to reach Bitrise API and failed.")
+                if counter == 4 {
+                    print("Bitrsie API replies with 503 error and may be temporarily down. Please try later.")
+                }
+                sleep(pollingInterval)
+            } else {
+                if returnCode != 200 { print("Response code for checkBuildStatus was ", returnCode) }
+                break
+            }
+            responseData = responseBody
+        }
+        return responseData
     }
-  }
-  
-  // Documented at http://devcenter.bitrise.io/api/v0.1/#get-appsapp-slugbuildsbuild-sluglog
-    public func getLogInfo(slug buildSlug: String) -> BitriseLogInfoResponse? {
-      let endpoint = "https://api.bitrise.io/v0.1/apps/\(slug)/builds/\(buildSlug)/log"
-      let headers = ["Content-Type": "application/json", "Authorization": theAccessToken]
-        
-      let request = delegate.request(url: endpoint, method: .get, headers: headers, body: nil)
-      let (responseData, response, responseError) = delegate.sendRequest(request: request)
-      
-      // TODO: CHeck guard statement
-      guard responseError == nil else {
-        print(responseError!)
-        exit(1)
-      }
-      
-      if let res = response as? HTTPURLResponse {
-        let returnCode = res.statusCode
-        if returnCode != 200 { print("Response code from `get log info` endpoint was ", returnCode) }
-      } else {
-        print("response returned nil")
-      }
-      
-      // APIs usually respond with the data you just sent in your GET request
-      guard let data = responseData, String(data: data, encoding: .utf8) != nil else {
-        print("no readable data received in response")
-        return nil
-      }
-        // For logging
-        // print("response: ", utf8Representation)
-        
-      do {
-        let decoder = JSONDecoder()
-        let res = try decoder.decode(BitriseLogInfoResponse.self, from: data)
-        return res
-      } catch {
-        print(error)
-        return nil
-      }
-  }
-  
-  // No special headers should be added for the request to the expiring_raw_log_url.
-  // See http://devcenter.bitrise.io/api/v0.1/#get-appsapp-slugbuildsbuild-sluglog
-  public func getLogs(from logInfo: BitriseLogInfoResponse) -> String? {
-    guard let url = logInfo.expiringRawLogURL else {
-      print(":!ERROR - Could not get the expiring raw log url!")
-      exit(1)
-    }
-    let request = delegate.request(url: url, method: .get, headers: nil, body: nil)
-    let (responseData, response, responseError) = delegate.sendRequest(request: request)
-        
-    // TODO: CHeck guard statement
-    guard responseError == nil else {
-      print(responseError!)
-      exit(1)
-    }
-        
-    if let res = response as? HTTPURLResponse {
-      let returnCode = res.statusCode
-      if returnCode != 200 { print("Response code from GET request to raw log url endpoint was ", returnCode) }
-    } else {
-      print("response returned nil")
-    }
-        
-    // APIs usually respond with the data you just sent in your GET request
-    if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
-      return utf8Representation
-    } else {
-      print("no readable data received in response")
-      return nil
-    }
-  }
-  
-  // # Status codes (status) and related status texts (status_text):
-  // # status=0: Not finished yet.
-  // # If is_on_hold = true: the build did not start yet (status_text=on-hold)
-  // # If is_on_hold = false: the build is running (status_text=in-progress).
-  // # status=1: Build finished, with success (status_text=success).
-  // # status=2: Build finished, with error (status_text=error).
-  // # status=3: Build was aborted (status_text=aborted).
-  public  func buildStatusMessage(_ status: Int) -> String {
-    switch status {
-    case 0:  return "Build TIMED OUT base on mobile trigger internal setting"
-    case 1:  return "Build finished, with success"
-    case 2:  return "Build finished, with error"
-    case 3:  return "Build was aborted"
-    default: return "Build status did not match any of the values 0, 1, 2 or 3.\n This is very strange!"
-    }
-  }
 }
 
 // MARK: Mock Client for testing
@@ -228,12 +221,11 @@ extension BitriseClient {
 // testing from the command line, i.e. `swift test`
 // #if DEBUG
 extension BitriseClient {
-  public init() {
-    self.theAccessToken = "dummy_theAccessToken"
-    self.token = "dummy_api_token"
-    self.slug = "fake_slug_4243534"
-    self.delegate = HTTPRequest()
-  }
-
+    public init() {
+        self.theAccessToken = "dummy_theAccessToken"
+        self.token = "dummy_api_token"
+        self.slug = "fake_slug_4243534"
+        self.delegate = HTTPRequest()
+    }
 }
 // #endif
